accfc559fe20ad2331cd638ba9fa4ff263358c07
Ty Williams <tywill8888@gmail.com>
2014-10-09 21:20:57 -0400

    linking MenuHead to index
    Linked MenuHead.php to Template.php

---
+++ MenuHead.php
@@ -0,0 +1,18 @@
+<script type="text/javascript"></script>
+
+<table>
+	<tr>
+		<td>
+			Home
+		</td>
+		<td>
+			Your Team
+		</td>
+		<td>
+			Your League 
+		</td>
+		<td>
+			Fourms and Contact
+		</td>
+	</tr>
+</table>
---
+++ Template.php
@@ -0,0 +1,20 @@
+<html>	
+	<head>
+		<script src=".js"></script>
+		<link type="text/style">
+		<?php
+			//check user requirments (if they are signed in) by setting a global variable /p/
+			//true or false. ext. if(user = paid acount){global $Paid=true}
+		?>
+	</head>
+	
+	<body>
+		<div class = "HeadMenu" id="HeadMenu">
+			<?php
+				#include 'HeadMenu.php';
+			?>
+			Hi
+		</div>
+	</body>
+
+</html>
